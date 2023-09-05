07a69eb5da643808e3db6cd8cfe503f34d0742f7
jiin <ji961118@gmail.com>
2023-09-05 18:04:00 +0900

    230905

--- resources/js/main.js
+++ resources/js/main.js
@@ -24,6 +24,65 @@ $(".main_visual_pause").click(function () {
     $(".main_visual_play").addClass("on");
 });
 
-$('.quickmenu_wrapper').slick({
-    slidesToShow : 10
+$(".quickmenu").slick({
+  slidesToShow: 10,
+  infinite: false,
+  arrows : false,
+  prevArrow : '.arrow_board_prev',
+  nextArrow : '.arrow_board_next',
+  responsive: [
+    {
+      breakpoint: 1750,
+      settings: {
+        slidesToShow: 7,
+        arrows : true,
+      },
+    },
+    {
+      breakpoint: 1300,
+      settings: {
+        slidesToShow: 6,
+        arrows : true,
+      },
+    },
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 5,
+        arrows : true,
+      },
+    },
+    {
+      breakpoint: 960,
+      settings: {
+        slidesToShow: 4,
+        arrows : true,
+      },
+    },
+    {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 3,
+        arrows : true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        arrows : true,
+      },
+    },
+  ],
+});
+
+$('.imgbanner').slick({	 
+    slidesToShow : 1,
+    slidesToScroll : 1,
+    infinite: false,
+    autoplay : true,
+    autoplaySpeed : 2000,
+    arrows : true,
+    prevArrow : '.arrow_board_prev',
+    nextArrow : '.arrow_board_next',
 });
